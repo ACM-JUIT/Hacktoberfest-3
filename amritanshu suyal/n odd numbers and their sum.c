25fe87244583bc7dc15fbaadb2675eedc659c61b
suyalamritanshu <72314518+suyalamritanshu@users.noreply.github.com>
2020-10-04 22:53:01 +0530

    Create n odd numbers and their sum.c

---
+++ amritanshu suyal/n odd numbers and their sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+
+
+{
+    int i;
+int j;
+    int sum;
+    scanf("%d",&j);
+    for(i=1;i<=j;i++)
+    printf("%d\n",i++);
+{
+
+    sum = 0;
+    for(i=1;i<=j;i++)
+    sum = sum + i;
+}
+printf(" sum = %d",sum);
+
+    return 0;
+}
