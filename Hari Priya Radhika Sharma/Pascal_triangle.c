ba253ad4567af948b6243f55f9c03a29fbeb3a25
haripriya75 <72337757+haripriya75@users.noreply.github.com>
2020-10-05 12:34:05 +0530

    Update Pascal_triangle.c
    Added comment

--- Hari Priya Radhika Sharma/Pascal_triangle.c
+++ Hari Priya Radhika Sharma/Pascal_triangle.c
@@ -1,9 +1,10 @@
+//This program is to print a pascal triangle
 #include <stdio.h>
 int main()
 {
    int rows, co = 1, space, i, j;
    printf("Enter the number of rows: ");
-   scanf("%d", &rows);
+   scanf("%d", &rows); //Add number of rows
    for (i = 0; i < rows; i++)
    {
       for (space = 1; space <= rows - i; space++)
