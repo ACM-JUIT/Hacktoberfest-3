04f7221831e6e3e4b1ec88a6e430fd7d10355bfb
parth24917 <72308605+parth24917@users.noreply.github.com>
2020-10-06 21:00:57 +0530

    Create fibonacci.c

---
+++ Parth/fibonacci.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+int n,a=0,b=1,x;
+printf("Enter number of terms = ");
+scanf("%d",&n);
+
+for(int i = 0;i<=n;i++)
+{
+printf(" %d ",a);
+x=a+b;
+a=b;
+b=x;
+}
+return 0 ;
+}
