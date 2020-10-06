efab6ee393b250811ac10520b669d816799380de
Sristiagarwal <72457516+Sristiagarwal@users.noreply.github.com>
2020-10-07 01:17:02 +0530

    Create TemperatureConversion.c
    Initial Commit.
    Temprature conversion calculator f to c.

---
+++ SristiAgarwal/TemperatureConversion.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+   float a,b;
+   printf("Enter temperature in farenheit ");
+   scanf("%f",&a);
+   b=(5.0/9)*(a-32);
+   printf("Temperature in Celcius is %f",b);
+
+    return 0;
+}
